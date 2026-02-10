c7af0b65ac2c53ce15fb3017e08969b8a0e97154
DaniAgredo <de287263@gmail.com>
2026-02-10 03:30:48 -0500

    Update site

---
+++ app.py
@@ -0,0 +1,15 @@
+﻿from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+
+@app.route("/conocenos")
+def conocenos():
+    return render_template("conocenos.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
